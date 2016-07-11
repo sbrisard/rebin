a3b6c642f2df5d0b0177da51348f7819daeb7fab
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-07-11 08:50:02 +0200

    Various corrections to unit tests.

--- rebin/tests/test_rebin.py
+++ rebin/tests/test_rebin.py
@@ -3,16 +3,16 @@ import unittest
 import numpy as np
 import numpy.testing as nptesting
 
-from rebin import rebin
+from rebin.rebin import rebin
 
 
 class TestRebin(unittest.TestCase):
     def setUp(self):
-        np.seed(20160711)
+        np.random.seed(20160711)
 
     def test_rebin(self):
         shape = (24, 18)
-        a = 2*np.rand(24, 18)-1
+        a = 2*np.random.rand(24, 18)-1
         bin_shape = (4, 3)
         actual = rebin(a, bin_shape)
         n0, n1 = (i//j for i, j in zip(shape, bin_shape))
@@ -22,7 +22,7 @@ class TestRebin(unittest.TestCase):
             for i1 in range(n1):
                 for j0 in range(bin0):
                     for j1 in range(bin1):
-                        expected[i0, i1] += a[row*i0+j0, col*i1+j1]
+                        expected[i0, i1] += a[bin0*i0+j0, bin1*i1+j1]
         nptesting.assert_array_equal(expected, actual)
 
 
