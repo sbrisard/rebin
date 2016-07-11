343c664dbbff0bb8b0b68a35453b510234dbaafe
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-07-11 08:52:46 +0200

    Packaging.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='rebin',
+    version='0.1.0',
+    description="Python/numpy implementation of IDL's rebin function.",
+    url='https://github.com/sbrisard/rebin',
+    author='Sébastien Brisard',
+    author_email='pypa-dev@googlegroups.com',
+    packages=['rebin'],
+    license='BSD-3',
+)
