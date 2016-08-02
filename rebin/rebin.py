8d45c4262439dc6aa729e3a26d8b7aa7c43fc946
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-08-02 09:17:39 +0200

    Basic implementation of rebin.

--- rebin/rebin.py
+++ rebin/rebin.py
@@ -1,4 +1,17 @@
 import numpy as np
 
-def rebin(a, bins):
-    pass
+from numpy.lib.stride_tricks import as_strided
+
+
+def rebin(a, bins, func=np.mean):
+    dim = a.ndim
+    if np.isscalar(bins):
+        bins = dim*(bins,)
+    elif len(bins) != dim:
+        raise ValueError('length of bins must be {} (was {})'
+                         .format(dim, len(bins)))
+
+    new_shape = [n//b for n, b in zip(a.shape, bins)]+list(bins)
+    new_strides = [s*b for s, b in zip(a.strides, bins)]+list(a.strides)
+    aa = as_strided(a, shape=new_shape, strides=new_strides)
+    return func(aa, axis=tuple(range(-dim, 0)))
