73f5853c637290314c9b402f465bf5b087d0caa7
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-08-03 07:50:18 +0200

    Check that bin sizes are ints.

--- rebin/rebin.py
+++ rebin/rebin.py
@@ -11,6 +11,10 @@ def rebin(a, bins, func=np.mean):
     elif len(bins) != dim:
         raise ValueError('length of bins must be {} (was {})'
                          .format(dim, len(bins)))
+    for b in bins:
+        if b != int(b):
+            raise ValueError('bins must be an int or a tuple of ints (was {})'.
+                             format(b))
 
     new_shape = [n//b for n, b in zip(a.shape, bins)]+list(bins)
     new_strides = [s*b for s, b in zip(a.strides, bins)]+list(a.strides)
