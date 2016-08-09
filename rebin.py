1e3c84373e182118315a02891d9c51ce37af0082
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-08-09 07:55:06 +0200

    Docstrings.

--- rebin.py
+++ rebin.py
@@ -14,16 +14,40 @@ def rebin(a, bins, func=np.mean):
     """Aggregate data from the input array `a` into rectangular bins
 
     The output array results from tiling `a` and applying `func` to each
-    tile. `bins` specifies the size of the tiles.
-
-    More precisely, the returned array `out` is such that::
+    tile. `bins` specifies the size of the tiles. More precisely, the
+    returned array `out` is such that::
 
         out[i0, i1, ...] = func(a[b0*i0:b0*(i0+1), b1*i1:b1*(i1+1), ...])
 
     If `bins` is an integer-like scalar, then ``b0 = b1 = ... = bins``
     in the above formula. If `bins` is a sequence of integer-like
-    scalars, then ``bins = (b0, b1, ...)`` and the length of `bins`
-    must equal the number of dimensions of `a`.
+    scalars, then ``b0 = bins[0]``, ``b1 = bins[1]``, ... and the length
+    of `bins` must equal the number of dimensions of `a`.
+
+    The reduction function `func` must accept an `axis` argument.
+    Examples of such function are
+
+      - ``numpy.mean`` (default),
+      - ``numpy.sum``,
+      - ``numpy.product``,
+      - ...
+
+    The following example shows how a (4, 6) array is reduced to a (2, 2)
+    array
+
+    >>> import numpy
+    >>> from rebin import rebin
+    >>> a = numpy.arange(24).reshape(4, 6)
+    >>> rebin(a, bins=(2, 3), func=numpy.sum)
+    array([[ 24,  42],
+           [ 96, 114]])
+
+    If the elements of `bins` are not integer multiples of the dimensions
+    of `a`, the remainding cells are discarded.
+
+    >>> rebin(a, bins=(2, 2), func=numpy.sum)
+    array([[16, 24, 32],
+           [72, 80, 88]])
 
     """
     a = np.asarray(a)
