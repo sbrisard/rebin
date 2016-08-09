50c3544423ba45e8463dffba7334712f14e287f1
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-08-09 09:36:36 +0200

    Mocking numpy for building docs on RTD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,3 +60,14 @@ texinfo_documents = [
    "Python/NumPy implementation of IDL's rebin function.",
    'Miscellaneous'),
 ]
+
+if on_rtd:
+    from unittest.mock import MagicMock
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return Mock()
+
+    MOCK_MODULES = ['numpy', 'numpy.lib.stride_tricks']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
