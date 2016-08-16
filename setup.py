49d503cb5c464bce14c4abed9b479ada63ef46a6
Sébastien Brisard <sebastien.brisard@m4x.org>
2016-08-16 08:13:57 +0200

    u'' → ''

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('rebin.py', 'r') as f:
                         lines, re.MULTILINE).group(1)
     description = re.search(r'^\"\"\"(.*)',
                             lines, re.MULTILINE).group(1)
-    author = re.search(r'^__author__\s*=\s*u[\'"]([^\'"]*)[\'"]',
+    author = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]',
                        lines, re.MULTILINE).group(1)
 
 setup(
